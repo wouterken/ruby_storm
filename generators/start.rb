3684b50680484140e57f20208597316f44fded2e
Wouter Coppieters <wouter.coppieters@youdo.co.nz>
2015-03-19 10:45:14 +1300

    Added environment messages on app start

--- generators/start.rb
+++ generators/start.rb
@@ -3,8 +3,12 @@ require 'fileutils'
 module Storm
   class Start
     def self.start(args)
+
       Storm.load_environment
+
       project_name = File.basename(Dir.pwd)
+      puts "Loading project [#{project_name}]"
+
       class_name = Inflector::classify(project_name)
       Inflector::constantize(class_name).main(STORM_ENV, ARGV[1..-1])
     end
