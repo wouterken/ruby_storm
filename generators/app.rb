2660bd384f36bb630bc35bdd3849f36fa4253e8a
Wouter Coppieters <wouter.coppieters@youdo.co.nz>
2015-03-26 12:41:03 +1300

    Load base level files to give chance to offset load order. Allow '-' in dirnames

--- generators/app.rb
+++ generators/app.rb
@@ -5,7 +5,7 @@ module Storm
     def self.start(args)
       Storm::connect
       project_name = File.basename(Dir.pwd)
-      class_name = Inflector::classify(project_name)
+      class_name = Inflector::classify(project_name.gsub("-","_"))
       Inflector::constantize(class_name).send(ARGV[1], ARGV[2..-1])
     end
   end
